38de109503a0f6207334f4e4ac57827e43f76281
Nurgazy Nazhimidinov <nurgasemetey@gmail.com>
2021-11-05 17:59:03 +0300

    add percentage

--- background.js
+++ background.js
@@ -8,11 +8,13 @@ document.addEventListener('DOMContentLoaded', function () {
           code: `
           var totalProblems = document.getElementsByClassName('css-alevek').length;
           var solvedProblems = document.getElementsByClassName('text-success fa fa-check css-alevek').length;
+          var perc = ((solvedProblems/totalProblems) * 100).toFixed(0);
 
           var el = document.createElement('div');
           el.setAttribute('class', 'panel-heading');
           el.setAttribute('id', 'leetcode-stat-extension-id')
-          el.innerText="Problems solved:"+solvedProblems+"/"+totalProblems;
+          el.innerText="Problems solved: "+solvedProblems+"/"+totalProblems+" - " + perc+"%";
+
           
           document.getElementsByClassName("panel-heading")[2].parentNode.insertBefore(el, document.getElementsByClassName("panel-heading")[2].nextSibling);
           `
@@ -41,11 +43,13 @@ document.addEventListener('DOMContentLoaded', function () {
           code: `
           var totalProblems = document.getElementsByClassName('css-alevek').length;
           var solvedProblems = document.getElementsByClassName('text-success fa fa-check css-alevek').length;
+          var perc = ((solvedProblems/totalProblems) * 100).toFixed(0);
 
           var el = document.createElement('div');
           el.setAttribute('class', 'panel-heading');
           el.setAttribute('id', 'leetcode-stat-extension-id')
-          el.innerText="Problems solved:"+solvedProblems+"/"+totalProblems;
+          el.innerText="Problems solved: "+solvedProblems+"/"+totalProblems+" - " + perc+"%";
+
           
           document.getElementsByClassName("panel-heading")[2].parentNode.insertBefore(el, document.getElementsByClassName("panel-heading")[2].nextSibling);
 
